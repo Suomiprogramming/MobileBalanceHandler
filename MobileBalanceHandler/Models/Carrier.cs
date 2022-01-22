efa7b657edb3c29e8b4ee4afa64245790ddabd82
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-01-22 13:09:49 +0600

    Добавил класс для того, чтобы при считывании конфигурационного файла, данные об операторах были представлены в виде объектов данного класса

---
+++ MobileBalanceHandler/Models/Carrier.cs
@@ -0,0 +1,10 @@
+namespace MobileBalanceHandler.Models
+{
+    public class Carrier
+    {
+        public string Name { get; set; }
+        public string[] Prefix { get; set; }
+        public string RequestType { get; set; }
+        public string Url { get; set; }
+    }
+}
