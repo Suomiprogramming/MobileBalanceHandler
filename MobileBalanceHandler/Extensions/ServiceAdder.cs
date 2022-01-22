863c379744e1166b538cf6ce3b6b8bf1e25a7bad
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-01-22 15:53:17 +0600

    Добавил регистрацию сервисов

---
+++ MobileBalanceHandler/Extensions/ServiceAdder.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using MobileBalanceHandler.Services.CarrierServices;
+using MobileBalanceHandler.Services.PaymentServices;
+using MobileBalanceHandler.Services.RequestHandlerServices;
+using MobileBalanceHandler.Services.ServicesComposer;
+
+namespace MobileBalanceHandler.Extensions
+{
+    public static class ServiceAdder
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IRequestHandler, SoapRequestHandler>();
+            services.AddTransient<IRequestHandler, HttpRequestHandler>();
+            services.AddTransient<IPaymentService, PaymentService>();
+            services.AddTransient<ICarrierService, CarrierService>();
+            services.AddTransient<IServicesComposer, ServicesComposer>();
+        }
+    }
+}
