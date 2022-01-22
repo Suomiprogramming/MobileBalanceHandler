a01d56c33b04b5d73c6744f4fbebfcd6e3f40243
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-01-22 15:55:11 +0600

    Добавил файлы миграции БД и снэпшот моделей

---
+++ MobileBalanceHandler/Migrations/20220119050528_PaymentsTableAdded.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MobileBalanceHandler.Migrations
+{
+    public partial class PaymentsTableAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    Sum = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
---
+++ MobileBalanceHandler/Migrations/20220122093404_PaymentDbMapWasAdded.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MobileBalanceHandler.Migrations
+{
+    public partial class PaymentDbMapWasAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Sum",
+                table: "Payments",
+                type: "numeric(7,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PaymentDate",
+                table: "Payments",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(2022, 1, 22, 15, 34, 4, 529, DateTimeKind.Local).AddTicks(4398),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Sum",
+                table: "Payments",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(7,2)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PaymentDate",
+                table: "Payments",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValue: new DateTime(2022, 1, 22, 15, 34, 4, 529, DateTimeKind.Local).AddTicks(4398));
+        }
+    }
+}
